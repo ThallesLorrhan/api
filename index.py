26d09e51d4a00b251230c10bf99d60aafc31277d
Thalles Lorrhan <thalles.lorrhan30@gmail.com>
2023-07-28 11:41:30 -0300

    add cors

--- index.py
+++ index.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure Cross Origin Resource Sharing (CORS) parameters.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/search/{loja}/{produto}')
 async def search(loja: str, produto: str):
